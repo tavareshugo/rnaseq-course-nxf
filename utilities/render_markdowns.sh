bbe3c7b479ba82dd4e1b059cdfcfab95515b3cee
Hugo Tavares <hm533@cam.ac.uk>
2023-11-01 16:48:09 +0000

    update slides and practicals

--- utilities/render_markdowns.sh
+++ utilities/render_markdowns.sh
@@ -1,6 +1,6 @@
 Rscript -e "rmarkdown::render('Markdowns/index.md', output_format = 'html_document', output_dir = 'docs')"
 
-for i in Markdowns/02_FastQC_practical.Rmd Markdowns/03_Quantification_with_Salmon_practical.Rmd Markdowns/04_Quality_Control_practical.Rmd Markdowns/05_Data_Exploration.Rmd Markdowns/07_Linear_Models.Rmd Markdowns/08_DE_analysis_with_DESeq2.Rmd Markdowns/09_Annotation_and_Visualisation.Rmd Markdowns/10_Gene_set_testing.Rmd
+for i in Markdowns/02_FastQC_practical.Rmd Markdowns/03_Quantification_with_Salmon_practical.Rmd Markdowns/04_Quality_Control_practical.Rmd Markdowns/05_Data_Exploration.Rmd Markdowns/04b_nextflow_rnaseq_practical.Rmd Markdowns/07_Linear_Models.Rmd Markdowns/08_DE_analysis_with_DESeq2.Rmd Markdowns/09_Annotation_and_Visualisation.Rmd Markdowns/10_Gene_set_testing.Rmd
 do
 
   Rscript -e "rmarkdown::render('$i', output_format = 'html_document', output_dir = 'docs')"
